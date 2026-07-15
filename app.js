d354b14af8650cd68dbfe7f48d0616cf76dedb31
Your Name <baroorkarsanjay@gmail.com>
2026-07-14 20:53:51 -0500

    Fix HTML template in app.js

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end(
-    <html>
-      <head>
-        <title>CloudBees CI Demo</title>
-      </head>
-      <body style="font-family: Arial; text-align: center; margin-top: 80px;">
-        <h1>CloudBees CI Demo Application</h1>
-        <h2>Deployment successful!</h2>
-        <p>Built with CloudBees CI and deployed to Amazon EKS.</p>
-      </body>
-    </html>
-  );
+
+  response.end(`
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CloudBees CI Demo</title>
+</head>
+<body style="font-family: Arial; text-align:center; margin-top:80px;">
+    <h1>CloudBees CI Demo Application</h1>
+    <h2>Deployment Successful!</h2>
+    <p>This application was built using CloudBees CI and deployed to Amazon EKS.</p>
+</body>
+</html>
+`);
 });
 
 server.listen(port, '0.0.0.0', () => {
-  console.log(Application is running on port );
+  console.log(`Application is running on port ${port}`);
 });
